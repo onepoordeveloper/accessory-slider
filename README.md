bb01c5a743554c1124c5c969a7178f874c8c1a14
Ahmad <onepoordeveloper@gmail.com>
2023-08-19 15:16:40 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Deployment
+
+1. Install dependencies `npm i`
+1. Run server `npm run serve`
+1. Visit [http://localhost:3000](http://localhost:3000)
+1. Enjoy!
---
+++ dist/script.js
@@ -0,0 +1,123 @@
+'use strict'
+const products = [
+  {
+    name: 'Clip x3',
+    price: 11,
+    image: '../assets/asset.png',
+  },
+  {
+    name: 'Clip XL x3',
+    price: 22,
+    image: '../assets/asset.png',
+  },
+  {
+    name: 'Spare charging cable',
+    price: 33,
+    image: '../assets/asset.png',
+  },
+  {
+    name: 'Spare charging cablerg with extra long title',
+    price: 44,
+    image: '../assets/asset.png',
+  },
+  {
+    name: 'this one is a placeholder',
+    price: 55,
+    image: '../assets/asset.png',
+  },
+]
+const listingWrapper = document.querySelector('.listing-wrapper')
+const scrollBar = document.querySelector('.scroll-bar')
+const statusBar = document.querySelector('.status')
+const cart = []
+const handleAddToCart = (event, product) => {
+  const target = event.currentTarget
+  // disable button, change text and show status
+  target.setAttribute('disabled', 'disabled')
+  const tempButtonText = target.innerText
+  target.innerText = 'Added!'
+  statusBar === null || statusBar === void 0
+    ? void 0
+    : statusBar.classList.remove('hidden')
+  // reset after 2 seconds
+  setTimeout(() => {
+    target.innerText = tempButtonText
+    target.removeAttribute('disabled')
+    statusBar === null || statusBar === void 0
+      ? void 0
+      : statusBar.classList.add('hidden')
+  }, 2000)
+  cart.push(product)
+  console.clear()
+  console.log(cart)
+}
+const addProductsToDOM = () => {
+  products.map((product) => {
+    // initializing building blocks
+    const item = document.createElement('div')
+    const contentWrapper = document.createElement('div')
+    const img = document.createElement('img')
+    const title = document.createElement('h2')
+    const price = document.createElement('span')
+    const button = document.createElement('button')
+    // adding classes to item
+    item.classList.add(
+      'rounded',
+      'bg-gray-100',
+      'p-4',
+      'min-w-[14rem]',
+      'flex',
+      'flex-col',
+      'justify-between'
+    )
+    // adding image src
+    img.src = product.image
+    // adding title and it's classes
+    title.innerText = product.name
+    title.classList.add('text-gray-800')
+    // adding price and it's classes
+    price.innerText = `€ ${product.price}`
+    price.classList.add('text-gray-500', 'text-sm')
+    // adding button, it's classes and event handler
+    button.classList.add(
+      'bg-blue-500',
+      'rounded-full',
+      'w-full',
+      'py-2',
+      'text-white',
+      'mt-4',
+      'disabled:opacity-75',
+      'transition',
+      'duration-300'
+    )
+    button.textContent = 'Add to cart'
+    button.addEventListener('click', (event) => handleAddToCart(event, product))
+    // appending building blocks to DOM
+    contentWrapper.append(img, title, price)
+    item.append(contentWrapper)
+    item.append(button)
+    listingWrapper === null || listingWrapper === void 0
+      ? void 0
+      : listingWrapper.append(item)
+  })
+}
+const scrollHandler = (event) => {
+  const target = event.target
+  const maxScroll = target.scrollWidth - target.clientWidth
+  const currentScroll = target.scrollLeft
+  const scrollPercentage = (currentScroll / maxScroll) * 100
+  // move scroll bar against the scroll percentage
+  if (scrollBar) {
+    scrollBar.style.left = `calc(${scrollPercentage}% - 16px)`
+  }
+}
+const listenToScroll = () => {
+  listingWrapper === null || listingWrapper === void 0
+    ? void 0
+    : listingWrapper.addEventListener('scroll', scrollHandler)
+}
+const main = () => {
+  addProductsToDOM()
+  listenToScroll()
+}
+main()
